1460a83287e513d820a8e1153b1341826ad1c3ea
Nicholas Frush <nick@frushco.de>
2024-11-15 23:11:21 -0500

    feat: add example of validation in soap server

--- tolling/backend/apps/tolling-ingest/main.go
+++ tolling/backend/apps/tolling-ingest/main.go
@@ -2,21 +2,29 @@ package main
 
 import (
 	"encoding/xml"
+	"errors"
 	"fmt"
 	"net/http"
 
 	"github.com/globusdigital/soap"
+	"github.com/go-playground/validator/v10"
 )
 
 // FooRequest a simple request
 type FooRequest struct {
 	XMLName xml.Name `xml:"fooRequest"`
-	Foo     string
+	Foo     string   `validate:"required"`
 }
 
 // FooResponse a simple response
 type FooResponse struct {
 	Bar string
+}
+
+var validate *validator.Validate
+
+func init() {
+	validate = validator.New()
 }
 
 // RunServer run a little demo server
@@ -35,11 +43,22 @@ func RunServer() {
 		// OperationHandlerFunc - do something
 		func(request interface{}, w http.ResponseWriter, httpRequest *http.Request) (response interface{}, err error) {
 			fooRequest := request.(*FooRequest)
+			if err := validate.Struct(fooRequest); err != nil {
+				// Convert validation errors to error message
+				if validationErrors, ok := err.(validator.ValidationErrors); ok {
+					var errorMsg string
+					for _, e := range validationErrors {
+						errorMsg += fmt.Sprintf("Field: %s, Error: %s; ", e.Field(), e.Tag())
+					}
+					return nil, errors.New(errorMsg)
+				}
+				return nil, err
+			}
+
 			fooResponse := &FooResponse{
 				Bar: "Hello " + fooRequest.Foo,
 			}
-			response = fooResponse
-			return
+			return fooResponse, nil
 		},
 	)
 	err := http.ListenAndServe(":8080", soapServer)
